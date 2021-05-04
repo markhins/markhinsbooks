6cd8f42a41a951f4d11bf009913eef9879736f50
MARKHINS MEDIA <muhammedfavasfanu@gmail.com>
2021-05-04 15:04:26 +0530

    first commit

---
+++ pass.js
@@ -0,0 +1,15 @@
+function login()
+	{
+		var uname = document.getElementById("psd").value;
+		
+		if(uname =='markhinsblr')
+		{
+            
+            window.location = "/index1.html";
+		}
+		else 
+		{
+        	alert("wrong password");
+		}
+		
+	}	
